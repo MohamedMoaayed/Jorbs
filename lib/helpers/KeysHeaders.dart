ecf80389e15ab0907902bd8fe30dc7b2e3eb4b3f
MohammedMoaayed <mohamed.moayed@gmail.com>
2020-08-16 00:46:28 +0300

    addedkeys | you need to change some references

---
+++ lib/helpers/KeysHeaders.dart
@@ -0,0 +1,14 @@
+// In this class you can find all the keys and headers
+//This file .gitignored
+class Keys {
+  static const String keyForApiPlaces = "------------------------";
+}
+
+//Headers and Key for USAJOBS API
+class Headers {
+  static Map<String, String> headers = {
+    "Host": "data.usajobs.gov",
+    "User-Agent": "--------------------------",
+    "Authorization-Key": "-----"
+  };
+}
