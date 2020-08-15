a7ec60e61f8f4676ac6c6296f8b8efae23fa0e89
MohammedMoaayed <mohamed.moayed@gmail.com>
2020-08-16 01:07:16 +0300

    Variable dismissed

--- lib/bloc/jorbs_bloc.dart
+++ lib/bloc/jorbs_bloc.dart
@@ -30,23 +30,25 @@ class JorbsBloc extends Bloc<JorbsEvent, JorbsState> {
       var url =
           'https://jobs.github.com/positions.json?search=${SavedFilters.position}&location=${SavedFilters.location}';
       try {
-        final response = await http.get(url);
-        final extractedData = json.decode(response.body);
-        if (extractedData == null) {
+        var response = await http.get(url);
+        var _responseJson = json.decode(response.body);
+        if (_responseJson == null) {
           print('There is no jobs on that search');
         }
 
-        for (var extractedData in extractedData) {
+        for (var entry in _responseJson) {
           _filteredJobs.add(Job(
-            companyName: extractedData['company'],
-            date: extractedData['created_at'],
-            jobUrl: extractedData['url'],
-            location: extractedData['location'],
-            logoUrl: extractedData['company_logo'],
-            position: extractedData['title'],
+            companyName: entry['company'],
+            date: entry['created_at'],
+            jobUrl: entry['url'],
+            location: entry['location'],
+            logoUrl: entry['company_logo'],
+            position: entry['title'],
             provider: 'github',
           ));
         }
+
+        _responseJson = [];
       } catch (error) {
         throw (error);
       }
@@ -57,27 +59,30 @@ class JorbsBloc extends Bloc<JorbsEvent, JorbsState> {
         final response = await http.get(
             'https://data.usajobs.gov/api/search?Keyword=${SavedFilters.position}&&LocationName=${SavedFilters.location}',
             headers: Headers.headers);
-        var responseJson = json.decode(response.body);
+        var _responseJson = json.decode(response.body);
+
         // Function for triming the nested JSON we received.
-        responseJson.removeWhere((key, value) => key != "SearchResult");
+        _responseJson.removeWhere((key, value) => key != "SearchResult");
 
         // Making a list for only Jobs info.
         var _listOfMapsOfJobs =
-            responseJson['SearchResult']['SearchResultItems'];
+            _responseJson['SearchResult']['SearchResultItems'];
 
-        for (var index in _listOfMapsOfJobs) {
+        for (var entry in _listOfMapsOfJobs) {
           _filteredJobs.add(Job(
-            position: index['MatchedObjectDescriptor']["PositionTitle"],
-            companyName: index['MatchedObjectDescriptor']["OrganizationName"],
-            date: index['MatchedObjectDescriptor']["PositionStartDate"],
-            location: index['MatchedObjectDescriptor']
+            position: entry['MatchedObjectDescriptor']["PositionTitle"],
+            companyName: entry['MatchedObjectDescriptor']["OrganizationName"],
+            date: entry['MatchedObjectDescriptor']["PositionStartDate"],
+            location: entry['MatchedObjectDescriptor']
                 ["PositionLocationDisplay"],
-            jobUrl: index['MatchedObjectDescriptor']["PositionURI"],
+            jobUrl: entry['MatchedObjectDescriptor']["PositionURI"],
             logoUrl:
                 'https://fcw.com/-/media/GIG/FCWNow/Logos/USAJobs_logo.jpg', // This's a deafult image because USAJOBS don't Provide Logos.
             provider: 'US',
           ));
         }
+        _responseJson = [];
+        _listOfMapsOfJobs = [];
         /*
         Here we can add more Providers and fetch their Apis data 
         */
